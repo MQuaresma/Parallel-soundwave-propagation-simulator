34749558ffc1b1fc7882584c4892f3477cb3e083
MQuaresma <miguelquaresma97@gmail.com>
2018-11-13 23:30:13 +0000

    Local variable is now used instead of direct array access for intermediate calculations

--- src/stencil_seq_opt.c
+++ src/stencil_seq_opt.c
@@ -4,7 +4,7 @@
 #define ITERATIONS 5
 
 int main(){
-    double C[5], G[2][N][N], start;
+    double C[5], G[2][N][N], start, temp;
     int last_matrix=0;
 
     start=omp_get_wtime();
@@ -13,13 +13,14 @@ int main(){
         //one iteration
         for(int i=1; i<N-1; i++)
             for(int j=1; j<N-1; j++){
-                G[last_matrix][i][j]= C[0]*G[last_matrix][i][j];
+                temp= C[0]*G[last_matrix][i][j];
                 for(int k=1; k < 5; k++){
-                    if(j+k < N) G[last_matrix][i][j]+= C[k]*G[last_matrix][i][j+k];
-                    if(j-k >= 0) G[last_matrix][i][j]+= C[k]*G[last_matrix][i][j-k];
-                    if(i+k < N) G[last_matrix][i][j]+= C[k]*G[last_matrix][i+k][j];
-                    if(i-k >= 0) G[last_matrix][i][j]+= C[k]*G[last_matrix][i-k][j];
+                    if(j+k < N) temp+= C[k]*G[last_matrix][i][j+k];
+                    if(j-k >= 0) temp+= C[k]*G[last_matrix][i][j-k];
+                    if(i+k < N) temp+= C[k]*G[last_matrix][i+k][j];
+                    if(i-k >= 0) temp+= C[k]*G[last_matrix][i-k][j];
                 }
+                G[last_matrix][i][j]=temp;
             }
         
         last_matrix=!last_matrix;
