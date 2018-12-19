c82730035f670c57d73a09cc25e3a262e5dea20b
jcm300 <zeclmartins@gmail.com>
2018-12-19 14:32:50 +0000

    script updated

--- MPI/run.sh
+++ MPI/run.sh
@@ -6,7 +6,7 @@
 #PBS -q mei
 
 module load gcc/5.3.0
-module load gnu/openmpi_eth/1.8.4
+module load gnu/openmpi_eth/2.0.0
 #module load gcc/7.2.0
 
 cd PCP/StencilSim
@@ -20,7 +20,27 @@ OMP_NUM_THREADS=3
 while [ $OMP_NUM_THREADS -lt 66 ]; do
     NUM=0
 	while [ $NUM -lt 15 ]; do
-		mpirun -np $OMP_NUM_THREADS -mca btl self,vader,tcp bin/a.out >> times/mpi_res.txt
+		mpirun -bycore --oversubscribe -np $OMP_NUM_THREADS -mca btl self,vader,tcp bin/a.out >> times/mpi_res.txt
+        let NUM=NUM+1
+    done
+    let OMP_NUM_THREADS="(($OMP_NUM_THREADS-1)*2)+1"
+done
+
+OMP_NUM_THREADS=3
+while [ $OMP_NUM_THREADS -lt 66 ]; do
+    NUM=0
+	while [ $NUM -lt 15 ]; do
+		mpirun -bynode --oversubscribe -np $OMP_NUM_THREADS -mca btl self,vader,tcp bin/a.out >> times/mpi_res.txt
+        let NUM=NUM+1
+    done
+    let OMP_NUM_THREADS="(($OMP_NUM_THREADS-1)*2)+1"
+done
+
+OMP_NUM_THREADS=3
+while [ $OMP_NUM_THREADS -lt 66 ]; do
+    NUM=0
+	while [ $NUM -lt 15 ]; do
+		mpirun -byslot --oversubscribe -np $OMP_NUM_THREADS -mca btl self,vader,tcp bin/a.out >> times/mpi_res.txt
         let NUM=NUM+1
     done
     let OMP_NUM_THREADS="(($OMP_NUM_THREADS-1)*2)+1"
